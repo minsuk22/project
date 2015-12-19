006709674e05d81948a95a717df51911f9a9ab04
Minsuk Kim <minsuk22@gmail.com>
2015-12-19 11:44:16 +0900

    initial upload

---
+++ html/album.php
@@ -0,0 +1,90 @@
+<html>
+<head>
+<title>예준이 동영상!!!</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="user-scalable=no">
+<style type='text/css'>
+A:link    {color:white; text-decoration:none;}
+A:visited {color:red; text-decoration:none; }
+A:active  {color:blue; text-decoration:none; }
+A:hover   {color:blue; text-decoration:none; }
+</style>
+</head>
+<body bgcolor="black" text="white" width=400>
+<font size=7>
+<?
+	$num = $_GET["num"];
+	$dir = $_GET["dir"];
+	$mode = $_GET["mode"];
+	$target = $_GET["target"];
+	if ($mode == "") {
+		$mode = "cd";
+	}
+	if ($dir == "") {
+		$dir = "pi_disk/Yejun";
+	}
+	$files = scandir("$dir/");
+	$n = 1;
+
+	foreach ($files as $f) {
+		if(($f == '.') || ($f == '..') || ($f == 'www_music')) { 
+			continue;
+		}
+		if ($num == $n) {
+			// echo ("$mode  $dir $target is playing now...<br>\n");
+		}
+		$n = $n + 1;
+	}
+
+	if ($mode != "cd") {
+		$fd = fopen("data/www_music_message.txt", 'a+');
+		if ($mode == "exit" || $mode == "stop") {
+			fwrite ($fd, "mode:$mode\n");
+			$play_line = "<center><font size=7 color=#aaaaaa>$mode</font></center>";
+		} else {
+			if ($mode == "play_dir") {
+				fwrite ($fd, "mode:dir_play\n");
+				fwrite ($fd, "path:$dir/$target\n");
+				$play_line = "$target <font color=#aaaaaa>album is playing...</font>";
+			} else if ($mode == "file_play") {
+				$target_file = str_replace("_____and_____", "&", $target);
+				fwrite ($fd, "mode:file_play\n");
+				fwrite ($fd, "path:$dir/$target_file\n");
+				$play_line = "$target_file <font color=#aaaaaa> is playing...</font>";
+			}
+		}
+		fwrite ($fd, "\n");
+		fclose($fd);
+	}
+
+	echo ("<table border=0 cellspacing=0>");
+	echo ("<tr><td colspan=2><image src=pi_disk/album.jpg width=950></td></tr>\n");
+	echo ("<tr><td width=700 height=10></td><td width=220></td></tr>\n");
+	echo ("<tr><td colspan=2 bgcolor=#222222><center><font size=8>예준이 동영상!!!</font></center></td></tr>\n");
+	echo ("<tr><td width=700 height=40></td><td width=220></td></tr>\n");
+	echo ("<tr bgcolor=#333333><td colspan=2 width=700><font size=7><b>list : </b></font></td></tr>\n");
+	echo ("<tr><td width=700 height=20></td><td width=220></td></tr>\n");
+
+	$n = 1;
+	foreach ($files as $f) {
+		if(($f == '.') || ($f == '..') || ($f == 'www_music')) { 
+			continue;
+		}
+		if (is_dir("$dir/$f")) {
+			echo ("<tr><td width=700 hight=15><a href='index.php?mode=cd&num=$n&dir=$dir/$f&target=$f'             ><font size=7>$f</font></a></td>");
+			echo ("<td align=right  ><a href='index.php?mode=play_dir&num=$n&dir=$dir&target=$f'><font size=7>전체 재생</font></a></td></tr>\n");
+		} else {
+			$target_file = str_replace("&", "_____and_____", $f);
+			echo ("<tr><td width=700 colspan=2><a href='$dir/$target_file'><font size=7>$f</font></a></td></tr>\n");
+			// echo ("<td align=right  ></td></tr>\n");
+		}
+		$n = $n + 1;
+	}
+	echo ("</table>");
+	
+
+?>
+</font>
+
+</body>
+</html>
---
+++ www_music/www_music_mp.py
@@ -0,0 +1,131 @@
+#!/usr/bin/python
+
+import sys
+import os
+import subprocess
+import time
+
+print "hello python!!"
+
+print sys.argv[0]
+if len(sys.argv) > 1:
+	para = sys.argv[1]
+	if para == "kill":
+		os.system ("ps ax | grep mplayer   | grep -v grep >  ps_list.txt")
+		os.system ("ps ax | grep omxplayer | grep -v grep >> ps_list.txt")
+
+		fp = open ("ps_list.txt", "r")
+		print ("kill -------------------------")
+		for line in fp:
+			line = line.replace("\n", "")
+			ps = line.split()[0]
+			print line
+			print "kill %s"%ps
+			os.system ("kill %s"%ps)
+			
+		fp.close()
+		print ("kill end -------------------------")
+			
+	exit()
+
+
+
+wmf = "www/data/www_music_message.txt"
+keycon = "www/data/keycon.txt"
+path = "path"
+mode = ""
+if os.path.exists(keycon):
+	os.system("rm -f %s"%keycon)
+
+num = 0
+while(1):
+	if os.path.exists(keycon):
+		pipe.stdin.write("\x1b[C")
+		os.system("rm -f %s"%keycon)
+	if os.path.exists(wmf):
+		# print ("------------cat----------")
+		# os.system("cat %s"%wmf)
+		print ("-------------------------")
+		fp = open (wmf, "r")
+		um = {}
+		for line in fp:
+			line = line.replace("\n", "")
+			print line
+			ls = line.split(":")
+			if len(ls) > 1:
+				um[ls[0]] = ls[1]
+		fp.close()
+		os.system("rm -f %s"%wmf)
+
+		mode = um["mode"]
+		if um["mode"] == "exit":
+			os.system ("%s kill"%sys.argv[0])
+			# os.system ("ps ax | grep mplayer | awk '{print \"kill \" $1}' | tcsh")
+			# os.system ("ps ax | grep omxplayer | awk '{print \"kill \" $1}' | tcsh")
+			break
+		elif um["mode"] == "stop":
+			print "stop"
+			os.system ("%s kill"%sys.argv[0])
+			# os.system ("ps ax | grep mplayer | awk '{print \"kill \" $1}' | tcsh")
+			# os.system ("ps ax | grep omxplayer | awk '{print \"kill \" $1}' | tcsh")
+		elif um["mode"] == "dir_play":
+			print "dir play"
+			cur_path = "www/%s"%um[path]
+			os.system ("%s kill"%sys.argv[0])
+			# os.system ("ps ax | grep mplayer | awk '{print \"kill \" $1}' | tcsh")
+			# os.system ("ps ax | grep omxplayer | awk '{print \"kill \" $1}' | tcsh")
+			ls = os.listdir ("www/%s"%um["path"])
+			fp = open("play_list.lst", "w")
+			for l in ls:
+				fp.write ("%s/%s\n"%(cur_path, l))
+			fp.close()
+			pipe = subprocess.Popen ("mplayer -playlist play_list.lst & ", shell=True, stdin=subprocess.PIPE)
+			# play_list = os.listdir("www/%s"%um[path])
+			# print play_list
+			# play_list_cnt = len(play_list)
+			# os.system ("touch www/data/www_music.play_done")
+			# num = -1
+		elif um["mode"] == "file_play":
+			print "file play mode"
+			os.system ("%s kill"%sys.argv[0])
+			# os.system ("ps ax | grep mplayer | awk '{print \"kill \" $1}' | tcsh")
+			# os.system ("ps ax | grep omxplayer | awk '{print \"kill \" $1}' | tcsh")
+			print "--------------"
+			print um["path"]
+			print "--------------"
+			if um["path"].endswith("avi") or um["path"].endswith("mp4"):
+				# os.system ("/usr/bin/mplayer \"www/%s\"& "%(um["path"]))
+				# os.system ("omxplayer \"www/%s\"& "%(um["path"]))
+				pipe = subprocess.Popen ("omxplayer \"www/%s\"& "%(um["path"]), shell=True, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+				# os.system ("mplayer \"www/%s\"& "%(um["path"]))
+				print "h1"
+				print ("play process is running...")
+			else:
+				print "h1"
+				# os.system ("/usr/bin/mplayer \"www/%s\"& "%(um["path"]))
+				# os.system ("omxplayer \"www/%s\"& "%(um["path"]))
+				pipe = subprocess.Popen ("mplayer \"www/%s\"& "%(um["path"]), shell=True, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+				# os.system ("mplayer \"www/%s\"& "%(um["path"]))
+				print "h1"
+				print ("play process is running...")
+
+		
+	# if mode == "dir_play":
+	# 	if os.path.exists("www/data/www_music.play_done"):
+	# 		if num < play_list_cnt:
+	# 			num = num + 1
+	# 		else:
+	# 			num = 0
+	# 		print play_list[num]
+
+	# 		os.system("rm -f play_mp3.csh")
+	# 		play_csh = open ("play_mp3.csh", "w")
+	# 		play_csh.write ("#!/usr/bin/tcsh\n")
+	# 		play_csh.write ("/usr/bin/mplayer -p -o local \"www/%s/%s\" \n"%(um[path],play_list[num]))
+	# 		play_csh.write ("touch www/data/www_music.play_done\n\n")
+	# 		play_csh.close()
+	# 		os.system("rm -rf www/data/www_music.play_done")
+	# 		os.system("chmod 755 play_mp3.csh")
+	# 		os.system("./play_mp3.csh &")
+	os.system("sleep 1")
+
